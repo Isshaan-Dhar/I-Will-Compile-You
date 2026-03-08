c7c7b1513140187f4cc52c2028aad4e514550fca
Isshaan-Dhar <ishaan.dhar2004@gmail.com>
2026-03-08 15:04:09 +0530

    Update lexer.py

--- core/lexer.py
+++ core/lexer.py
@@ -1,19 +1,18 @@
 import re
 
-# Each pattern represents a "Token" type
 TOKEN_SPEC = [
-    ('NUMBER', r'\d+'),        # Matches digits
-    ('ASSIGN', r'='),          # Matches the assignment operator
-    ('PLUS',   r'\+'),         # Matches the addition operator
-    ('ID',     r'[A-Za-z]+'),  # Matches variable names (like x, y)
-    ('SKIP',   r'[ \t]+'),     # Ignores spaces and tabs
+    ('NUMBER', r'\d+'),
+    ('ASSIGN', r'='),
+    ('PLUS',   r'\+'),
+    ('ID',     r'[A-Za-z]+'),
+    ('SKIP',   r'[ \t]+'), 
 ]
 
 def tokenize(code):
-    """Breaks raw text into a stream of identified tokens."""
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in TOKEN_SPEC)
     for mo in re.finditer(tok_regex, code):
         kind = mo.lastgroup
         value = mo.group()
-        if kind != 'SKIP': # We don't need spaces for logic
+        if kind != 'SKIP':
+
             yield (kind, value)
