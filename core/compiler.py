9e05a693f90432a6cc08d9750e582d3793c500aa
Isshaan-Dhar <ishaan.dhar2004@gmail.com>
2026-03-08 15:02:32 +0530

    Update compiler.py

--- core/compiler.py
+++ core/compiler.py
@@ -1,10 +1,9 @@
 class Compiler:
     def compile(self, ast):
-        """Translates the AST into a list of low-level instructions."""
-        # We push numbers, add them, then store the result in the variable
         return [
             f"PUSH {ast['left']}", 
             f"PUSH {ast['right']}", 
             "ADD", 
             f"STORE {ast['name']}"
+
         ]
