e70a68046928bf9ea4e929135e927902fe841f98
Isshaan-Dhar <ishaan.dhar2004@gmail.com>
2026-03-08 15:05:47 +0530

    Update vm.py

--- core/vm.py
+++ core/vm.py
@@ -4,7 +4,6 @@ class VirtualMachine:
         self.vars = {}
 
     def run(self, bytecode):
-        """Processes each bytecode instruction one by one."""
         for instr in bytecode:
             parts = instr.split()
             cmd = parts[0]
@@ -12,9 +11,8 @@ class VirtualMachine:
             if cmd == "PUSH": 
                 self.stack.append(int(parts[1]))
             elif cmd == "ADD": 
-                # Pop the last two numbers, add them, push result back
                 self.stack.append(self.stack.pop() + self.stack.pop())
             elif cmd == "STORE": 
-                # Save the result from the top of the stack into a variable
                 self.vars[parts[1]] = self.stack.pop()
+
         return self.vars
