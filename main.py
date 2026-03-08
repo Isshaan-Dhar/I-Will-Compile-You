0e63677e6f1275da5ca10880a08c08a43ee48038
Isshaan-Dhar <ishaan.dhar2004@gmail.com>
2026-03-08 14:59:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ from core.parser import Parser
 from core.compiler import Compiler
 from core.vm import VirtualMachine
 
-# The high-level code we want to 'compile'
 code = "x = 10 + 20"
 
-# 1. Break into tokens
 tokens = tokenize(code)
 
-# 2. Build the Tree
 ast = Parser(tokens).parse()
 
-# 3. Generate Instructions
 bytecode = Compiler().compile(ast)
 
-# 4. Run the Machine
 result = VirtualMachine().run(bytecode)
 
 print(f"Compilation and Execution Successful!")
